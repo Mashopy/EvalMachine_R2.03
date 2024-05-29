317ce86dbd120c1217e328832c6b67b890bdb9e1
TheMalachite <eliasgheeraert@gmail.com>
2024-05-29 17:16:45 +0200

    EvalMachine: Création d'un programme permettant de saisir un tableau d'entier

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Saisir la taille du tableau:");
+int taille = Convert.ToInt32(Console.ReadLine());
+
+int[] T = TableauEntier(taille);
+
+static int[] TableauEntier(int taille)
+{
+    int[] T = new int[taille];
+
+    for (int i = 0; i < T.Length; i++)
+    {
+        Console.WriteLine($"Saisir l'élément {i}:");
+        T[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    
+    return T;
+}

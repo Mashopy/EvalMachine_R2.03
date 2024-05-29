6667d21bb84e11caca2f301d1cd9241d3068e94a
TheMalachite <eliasgheeraert@gmail.com>
2024-05-29 17:18:43 +0200

    EvalMachine: Ajout de sous-programmes permettants de créer un deuxième tableau en replacer un entier specifique par un autre

--- Program.cs
+++ Program.cs
@@ -2,6 +2,9 @@
 int taille = Convert.ToInt32(Console.ReadLine());
 
 int[] T = TableauEntier(taille);
+int premier = SaisieEntierPremier();
+int second = SaisieEntierSecond();
+int[] T2 = TableauResultat(T, premier, second);
 
 static int[] TableauEntier(int taille)
 {
@@ -14,4 +17,33 @@ static int[] TableauEntier(int taille)
     }
     
     return T;
+}
+
+static int SaisieEntierPremier()
+{
+    Console.WriteLine("Saisir l'élément à remplacer:");
+    int valeur = Convert.ToInt32(Console.ReadLine());
+
+    return valeur;
+}
+
+static int SaisieEntierSecond()
+{
+    Console.WriteLine("Saisir la valeur avec laquelle le remplacer:");
+    int valeur = Convert.ToInt32(Console.ReadLine());
+
+    return valeur;
+}
+
+static int[] TableauResultat(int[] tab, int premier, int second)
+{
+    int[] T2 = tab;
+
+    for (int i = 0; i < T2.Length; i++) {
+        if (T2[i] == premier) {
+            T2[i] = second;
+        }
+    }
+
+    return T2;
 }
